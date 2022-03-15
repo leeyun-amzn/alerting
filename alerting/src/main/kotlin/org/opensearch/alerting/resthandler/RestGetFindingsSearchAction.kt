18cd6d8ddee5cffba0658461bffb3d029b29b42a
Annie Lee <leeyun@amazon.com>
2022-03-15 13:54:03 -0700

    Use id.keyword field
    Signed-off-by: Annie Lee <leeyun@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/resthandler/RestGetFindingsSearchAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/resthandler/RestGetFindingsSearchAction.kt
@@ -51,7 +51,7 @@ class RestGetFindingsSearchAction : BaseRestHandler() {
             srcContext = FetchSourceContext.DO_NOT_FETCH_SOURCE
         }
 
-        val sortString = request.param("sortString", "id")
+        val sortString = request.param("sortString", "id.keyword")
         val sortOrder = request.param("sortOrder", "asc")
         val missing: String? = request.param("missing")
         val size = request.paramAsInt("size", 20)
