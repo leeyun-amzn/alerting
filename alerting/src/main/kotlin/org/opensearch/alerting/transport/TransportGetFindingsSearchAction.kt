cdacac614abc258271e5e2d6f1ea8697a64511e0
Annie Lee <leeyun@amazon.com>
2022-03-15 17:31:38 -0700

    Update TransportGetFindingsSearchAction.kt

--- alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetFindingsSearchAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetFindingsSearchAction.kt
@@ -84,7 +84,7 @@ class TransportGetFindingsSearchAction @Inject constructor(
             .fetchSource(FetchSourceContext(true, Strings.EMPTY_ARRAY, Strings.EMPTY_ARRAY))
             .seqNoAndPrimaryTerm(true)
             .version(true)
-        val queryBuilder = QueryBuilders.matchAllQuery()
+        val queryBuilder = QueryBuilders.query().matchAllQuery()
         // TODO: Update query to support other parameters of search
 
         if (!getFindingsSearchRequest.findingId.isNullOrBlank())
